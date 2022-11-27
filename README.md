c04857633bc22c43f24a3f583d3d9a8c3a973e8e
Ryan Sakurai <ryansakurai03@gmail.com>
2022-11-27 20:40:43 -0300

    readme written

---
+++ README.md
@@ -0,0 +1,9 @@
+# WAR CARD GAME
+
+Simulation of the card game war that gives stats at the end of the match, made while learning Python.
+
+- All 56 cards are used
+- The cards are shuffled only once, at the start of the match
+- When war happens, each player withdraws 3 cards
+
+![output](img/output.png)
--- game.py
+++ game.py
@@ -1,3 +1,13 @@
+"""
+WAR CARD GAME
+
+Simulation of the card game war that gives stats at the end of the match.
+
+- All 56 cards are used
+- The cards are shuffled only once, at the start of the match
+- When war happens, each player withdraws 3 cards
+"""
+
 from typing import Iterable, Tuple
 import entities
 
